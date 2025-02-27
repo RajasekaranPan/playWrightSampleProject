476fa4290263aa85c19209e31a19eb48033a0c13
RajasekaranP <rajasek93@gmail.com>
2025-02-27 22:57:07 +0530

    all handsons

---
+++ tests/example.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect, Browser, Page } from '@playwright/test';
+//import { chromium } from '@playwright/test';
+import { channel } from 'diagnostics_channel';
+
+test('has title', async ({page}) => {
+
+  // const browser: Browser = await chromium.launch({channel: 'chrome'});
+  // const page:Page = await browser.newPage();
+
+  await page.goto('https://playwright.dev/');
+
+  console.log("Raja");
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+
+  await page.screenshot({path: 'samplescreenshot.png'});
+});
